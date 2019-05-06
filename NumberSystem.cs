a44f4f46c26204bf5c72803fe200c88a5570d31a
Rajagopalkk <49686798+Rajagopalkk@users.noreply.github.com>
2019-05-06 07:16:34 +0530

    Add files via upload

---
+++ NumberSystem.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class NumberSystem
+    {
+        static void Main(string[] args)
+        {
+            //string firstName = "Hubsons";
+            //string lastName = "Clark";
+            //string PhoneNumber = "890898989";
+            //int age = 20;
+            //var propeties = new object[] { "Hubsons" , "Clark" , "890898989",20 };
+            //PringDetails(propeties);
+            //string inputstr1 = "Hello";
+            //HelloString(out inputstr1);
+            //CheckVsUncheck();
+            #region Pallindrom
+            // long numberToCheck = 0;
+            //Console.WriteLine($"Enter a Number to Check whether it's pallindrom or not ");
+            //var result = checkPallindromNumber(numberToCheck);
+            //Console.WriteLine($"Resule is :{result}");
+            #endregion
+
+            #region swap two numbers
+            Console.WriteLine("Enter number to check it is armstrong or not");
+            int numberToCheck = int.Parse(Console.ReadLine());
+            int temp = numberToCheck;
+            int reminder,sum=0;
+            while(numberToCheck > 0)
+            {
+                reminder = Convert.ToInt32(numberToCheck % 10);
+                sum = sum+(reminder*reminder*reminder);
+                numberToCheck = numberToCheck / 10;
+            }
+            if(numberToCheck==temp)
+            {
+                Console.WriteLine($"Entered {numberToCheck} is Armstrong number");
+            }
+            else
+            
+                Console.WriteLine($"Entered {numberToCheck} is Not Armstrong number");
+            
+            #endregion
+            Console.ReadLine();
+        }
+        //private static void CheckVsUncheck()
+        //{
+            //int x = int.MaxValue;
+            //int y = int.MaxValue;
+            //var z = x + y;
+            //checked
+            //{
+            //    int val = x + y;
+            //    Console.WriteLine(val + 2);
+            //}
+        //    unchecked
+        //    {
+        //        int val = x + y;
+        //        Console.WriteLine(val + 2);
+        //    }
+        //}
+
+        //private static string  checkPallindromNumber(long numberToCheck)
+        //{
+        //    var IsPallindromNumber = string.Empty;
+        //    numberToCheck = long.Parse(Console.ReadLine());
+        //    long temp = numberToCheck;
+        //    int reminder = 0; long sum = 0;
+        //    while (numberToCheck > 0)
+        //    {
+        //        reminder = Convert.ToInt32(numberToCheck % 10);
+        //        sum = sum * 10 + reminder;
+        //        numberToCheck = numberToCheck / 10;
+        //    }
+        //    if (sum == temp)
+        //         IsPallindromNumber = "Pallindrom Number";
+        //    else
+        //        IsPallindromNumber = "Not a Pallindrom Number";
+        //    return IsPallindromNumber;
+        //}
+
+       
+        //Ref Vs Out Vs Params
+        //public static void HelloString(out string inputstr)
+        //{
+        //    inputstr = "world";
+        //}
+        //public static void PringDetails(params object[] args)
+        //{
+        //    Console.WriteLine($" First Name :{args[0]}\n Last Name :{args[1]}\n PhoneNumber :{args[2]}\n Age :{args[3]}");
+        //}
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        /// <summary>
+        /// Read & Print the integer elements in an Array.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main_Renamed(string[] args)
+        {
+            //Console.WriteLine("What is the length of your array ?");
+            //int arrLength = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"The Length is {arrLength}");
+            ////Console.WriteLine("The Length is {1}",arrLength);
+            //int[] myIntegerArr = new int[arrLength];
+            #region Array Initialisation
+
+            //myIntegerArr[0] = 10;
+            //myIntegerArr[1] = 20;
+            //myIntegerArr[2] = 30;
+            //myIntegerArr[3] = 40;
+            //myIntegerArr[4] = 50;
+
+            //Console.WriteLine($"1st element : {myIntegerArr[0]} \t " +
+            //    $"2nd element : {myIntegerArr[1]}\t" +
+            //    $"3rd element : {myIntegerArr[2]}\t" +
+            //    $"4th element : {myIntegerArr[3]}\t" +
+            //    $"5th element : {myIntegerArr[4]}\t");
+            #endregion
+            #region Read from Console & display on console window
+            //for (int i = 0; i < arrLength; i++)
+            //{
+            //    Console.WriteLine($"Please enter {i} element : ");
+            //    myIntegerArr[i] = int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine($"The elements are : \t");
+            //for (int i = 0; i < arrLength; i++)
+            //{
+            //    Console.WriteLine(myIntegerArr[i]);
+            //}
+            // Console.ReadLine();
+            #endregion
+
+            #region additional
+
+            //SumOfAllElements();
+            #endregion
+            #region Reverse
+            //Console.WriteLine("please enter your number: ");
+            //int myInt = int.Parse(Console.ReadLine());
+            //int[] myArray = new int[myInt];
+            //for(int i=0; i<myInt; i++)
+            //{
+            //    Console.WriteLine($"please enter your number : {i} ");
+            //    myArray[i] = int.Parse(Console.ReadLine());
+            //}
+            //int reverse = myArray[0];
+            //for(int i=0; i<myInt; i--)
+            //{
+            //    reverse = myArray[i];
+            //}
+            //Console.WriteLine($"Reversed Number: {reverse}");
+            //Console.ReadLine();
+            #endregion
+
+            #region largest and smallest number
+            FindSmallestAndLargestNumber();
+
+            #endregion
+            Console.ReadLine();
+        }
+
+        private static void FindSmallestAndLargestNumber()
+        {
+            Console.WriteLine("Please enter the lenght of the Array: ");
+            int arrayLength = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The Entered length {arrayLength}");
+            int[] myInt = new int[arrayLength];
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.WriteLine($"Please enter the   {i + 1} number:");
+                myInt[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(myInt);     //Ascending Order
+            Array.Reverse(myInt);  // Descensing Order
+            Console.WriteLine($"Largest number: {myInt[arrayLength - 1]} Smallest Number :{myInt[0]}");
+        }
+        private static void SumOfAllElements()
+        {
+            Console.WriteLine("Please enter the lenght of the Array: ");
+            int arrayLength = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The Entered length {arrayLength}");
+            int[] myInt = new int[arrayLength];
+            for (int i = 0; i < arrayLength; i++)
+            {
+                Console.WriteLine($"Please enter the   {i + 1} number:");
+                myInt[i] = int.Parse(Console.ReadLine());
+            }
+            int sum = 0;    
+            for (int i = 0; i < arrayLength; i++)
+            {
+                sum = myInt[i] + sum;                    
+            }
+            Console.WriteLine($"Sum of all element: {sum}");
+        }
+    }
+}
